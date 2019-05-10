9d0e71fd680d33b349478bfd67ad9865bbda1d89
kavindu Gayantha <96kavindugayantha@gmail.com>
2019-05-10 06:30:05 +0530

    login successfull and logout function also app is almost done for now

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ firebase.auth().onAuthStateChanged(function(user) {
       // User is signed in.
       document.getElementById("user_div").style.display = "block";
       document.getElementById("login_div").style.display = "none";
+
+      var user = firebase.auth().currentUser;
+      if(user!=null) //that means still logged in
+      {
+        var email_id = user.email;
+        document.getElementById("user_para").innerHTML ="welcome user <br>" + email_id;
+      }
+
+
+
     } else {
       // No user is signed in.
       document.getElementById("user_div").style.display = "none";
@@ -17,5 +27,17 @@ function login()
     var userEmail = document.getElementById("email_field").value;
     var userPass = document.getElementById("password_field").value;
 
-    window.alert(userEmail + " " + userPass);
+    // window.alert(userEmail + " " + userPass);
+    firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // ...
+      window.alert("error : " + errorMessage);
+    });
+}
+function logout()
+{
+  firebase.auth().signOut();
+  
 }
